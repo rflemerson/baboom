1efbbceb9e458ef09ac93dd464e0a5c4c25e2454
Rafael E. Leal <rflemerson@gmail.com>
2025-12-25 15:11:32 -0300

    refact: to monorepo

--- infra/vars.tf
+++
@@ -1,6 +0,0 @@
-variable compartment_ocid { default = "ocid1.tenancy.oc1..aaaaaaaavf6wkrobxic77lywhdtbmojb24ppkuavgi77piyy62y4irzjrmka" }
-variable tenancy_ocid { default = "ocid1.tenancy.oc1..aaaaaaaavf6wkrobxic77lywhdtbmojb24ppkuavgi77piyy62y4irzjrmka" }
-variable oci_core_instance--source_details-source_id--export_instance-backend-baboom { default = "ocid1.image.oc1.sa-saopaulo-1.aaaaaaaaiebj2ui4fns446ovgvrp3fzx6cguarhhqpfb4t3dbci6ejpsk4sq" }
-variable availability_domain--udwV-SA-SAOPAULO-1-AD-1 { default = "udwV:SA-SAOPAULO-1-AD-1" }
-variable fault_domain--FAULT-DOMAIN-1 { default = "FAULT-DOMAIN-1" }
-variable region { default = "sa-saopaulo-1" }
