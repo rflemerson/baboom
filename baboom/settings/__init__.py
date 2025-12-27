0d4cfd980e989979aba4ca5ab25e251f803e978e
Rafael E. Leal <rflemerson@gmail.com>
2025-12-27 13:08:42 -0300

    config: update settings

--- baboom/settings/__init__.py
+++ baboom/settings/__init__.py
@@ -1,41 +1,7 @@
 import os
 
 from .base import *
-from .logs import *
-
-# Project apps
-INSTALLED_APPS += ["core"]
-
-# Whitenoise
-MIDDLEWARE.insert(
-    MIDDLEWARE.index("django.middleware.security.SecurityMiddleware") + 1,
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-)
-
-# Simple History
-INSTALLED_APPS += ["simple_history"]
-MIDDLEWARE += ["simple_history.middleware.HistoryRequestMiddleware"]
-
-# Treebeard
-INSTALLED_APPS += ["treebeard"]
-
-# Nested Admin
-INSTALLED_APPS += ["nested_admin"]
-
-# Django Filter
-INSTALLED_APPS += ["django_filters"]
-
-# HTMX
-INSTALLED_APPS += ["django_htmx"]
-MIDDLEWARE += ["django_htmx.middleware.HtmxMiddleware"]
-
-# Django Widget Tweaks
-INSTALLED_APPS += ["widget_tweaks"]
-
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+from .settings import *
 
 env_name = os.getenv("DJANGO_ENV", "development")
 
--- baboom/settings/settings.py
+++ baboom/settings/settings.py
@@ -1,4 +1,38 @@
-# AI generated file
+from .base import BASE_DIR, INSTALLED_APPS, MIDDLEWARE
+
+# Project apps
+INSTALLED_APPS += ["core"]
+
+# Whitenoise
+MIDDLEWARE.insert(
+    MIDDLEWARE.index("django.middleware.security.SecurityMiddleware") + 1,
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+)
+
+# Simple History
+INSTALLED_APPS += ["simple_history"]
+MIDDLEWARE += ["simple_history.middleware.HistoryRequestMiddleware"]
+
+# Treebeard
+INSTALLED_APPS += ["treebeard"]
+
+# Nested Admin
+INSTALLED_APPS += ["nested_admin"]
+
+# Django Filter
+INSTALLED_APPS += ["django_filters"]
+
+# HTMX
+INSTALLED_APPS += ["django_htmx"]
+MIDDLEWARE += ["django_htmx.middleware.HtmxMiddleware"]
+
+# Django Widget Tweaks
+INSTALLED_APPS += ["widget_tweaks"]
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Logging Configuration
 # Best Practice: Structured/JSON logging recommended for Production.
