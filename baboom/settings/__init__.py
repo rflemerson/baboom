165781afcc730ff30343a6ca9a70c3e672dffeea
Rafael E. Leal <rflemerson@gmail.com>
2026-01-24 23:45:08 -0300

    fix import order

--- baboom/settings/__init__.py
+++ baboom/settings/__init__.py
@@ -1,14 +1,8 @@
 import os
 
 from .apps.celery import *
-
-# 3. Add Component Configurations
 from .apps.logging import *
-
-# 2. Add Project Specific Configuration (Apps, Middleware, Static, Locale)
 from .apps.project import *
-
-# 1. Start with the Pure Django Base Settings
 from .base import *
 
 env_name = os.getenv("DJANGO_ENV", "development")
