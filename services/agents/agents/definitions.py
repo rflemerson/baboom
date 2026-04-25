553ee05cbb7a1964baf876a0462f66485fe5d389
Rafael E. Leal <rflemerson@gmail.com>
2026-04-25 19:10:57 -0300

    Register Dagster work queue sensor

--- services/agents/agents/definitions.py
+++ services/agents/agents/definitions.py
@@ -4,6 +4,7 @@ from dagster import Definitions, load_assets_from_modules
 
 from .defs import assets as assets_module
 from .defs.pipeline import build_process_item_job
+from .defs.sensors import work_queue_sensor
 
 all_assets = load_assets_from_modules([assets_module])
 
@@ -13,5 +14,5 @@ process_item_job = build_process_item_job(selection=all_assets)
 defs = Definitions(
     assets=all_assets,
     jobs=[process_item_job],
-    sensors=[],
+    sensors=[work_queue_sensor],
 )
